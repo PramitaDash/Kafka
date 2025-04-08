7a4a76180008093cf3c970f1248b7476d0196fa6
PramitaDash <dashpramita34@gmail.com>
2025-04-08 15:43:04 +0530

    first message

---
+++ SpringBootKafka/src/main/java/com/kafka/controller/MessageProducer.java
@@ -0,0 +1,24 @@
+package com.kafka.controller;
+
+import com.kafka.kafka.KafkaProducer;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/kafka")
+public class MessageProducer {
+
+    private KafkaProducer kafkaProducer;
+
+//    http:localhost:8080/api/v1/kafka/publish?message=hello world
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message){
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to the topic");
+    }
+}
